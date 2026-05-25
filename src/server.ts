b29c509e50447485fbcbbec0f872c6443ce6e763
hussainKhan001 <hussain@neotericgrp.in>
2026-05-25 11:18:44 +0530

    cors

--- src/server.ts
+++ src/server.ts
@@ -45,9 +45,7 @@ connectDB();
 initBroadcaster(server);
 
 // Middleware
-app.use(cors({
-  origin: "*",
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
